6d18066e369b8fdf13b4f287af62b0ae16273563
Stanlo06-arch <Stanlo06@web.de>
2026-08-20 00:10:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,38 +5,48 @@ const {
     GatewayIntentBits
 } = require('discord.js');
 
+// ==================================================
+// CLIENT
+// ==================================================
+
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMembers
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildVoiceStates
     ]
 });
 
-// ==========================================
+// ==================================================
 // EVENTS
-// ==========================================
+// ==================================================
 
 const ready = require('./events/ready');
 const guildMemberAdd = require('./events/guildMemberAdd');
 const voiceStateUpdate = require('./events/voiceStateUpdate');
 
-// ==========================================
+// ==================================================
 // FUNCTIONS
-// ==========================================
+// ==================================================
 
 const botStatus = require('./functions/botStatus');
 
-// ==========================================
-// START
-// ==========================================
+// ==================================================
+// EVENTS STARTEN
+// ==================================================
 
 ready(client);
 guildMemberAdd(client);
 voiceStateUpdate(client);
+
+// ==================================================
+// FUNCTIONS STARTEN
+// ==================================================
+
 botStatus(client);
 
-// ==========================================
+// ==================================================
 // LOGIN
-// ==========================================
+// ==================================================
 
 client.login(process.env.TOKEN);
