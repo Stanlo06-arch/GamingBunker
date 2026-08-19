49374cc1e50a4282a53ca35a8d27c44f88eaa099
Stanlo06-arch <Stanlo06@web.de>
2026-08-19 23:44:05 +0200

    Update guildMemberAdd event logging messages

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,12 +1,15 @@
 module.exports = (client) => {
 
+    console.log('✅ guildMemberAdd.js wurde geladen!');
+
     client.on('guildMemberAdd', async (member) => {
 
         console.log('━━━━━━━━━━━━━━━━━━━━━━━━━━━━');
-        console.log('🔥 NEUES MITGLIED ERKANNT!');
+        console.log('🔥 GUILDMEMBERADD AUSGELÖST!');
         console.log(`👤 User: ${member.user.tag}`);
         console.log(`🆔 User-ID: ${member.id}`);
         console.log(`🏠 Server: ${member.guild.name}`);
+        console.log(`🆔 Server-ID: ${member.guild.id}`);
         console.log('━━━━━━━━━━━━━━━━━━━━━━━━━━━━');
 
     });
