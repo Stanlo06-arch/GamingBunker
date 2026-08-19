fdd05606566393cb58c65a3566fb6e6da8f1c065
Stanlo06-arch <Stanlo06@web.de>
2026-08-19 23:22:43 +0200

    Initialize Discord client with events and intents

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+require('dotenv').config();
 
+const {
+    Client,
+    GatewayIntentBits,
+    Partials
+} = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers
+    ],
+    partials: [
+        Partials.User,
+        Partials.GuildMember
+    ]
+});
+
+// Events
+const ready = require('./events/ready');
+const guildMemberAdd = require('./events/guildMemberAdd');
+
+ready(client);
+guildMemberAdd(client);
+
+client.login(process.env.TOKEN);
