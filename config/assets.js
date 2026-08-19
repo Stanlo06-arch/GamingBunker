48129c38b0598529635eae906f08565db1a7b646
Stanlo06-arch <Stanlo06@web.de>
2026-08-19 23:36:48 +0200

    Create assets.js

---
+++ config/assets.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+    GAMINGBUNKER_LOGO:
+        'https://cdn.discordapp.com/attachments/1510626895627288676/1539748638106849290/ChatGPT_Image_30._Juli_2026_22_56_07.png?ex=6a87722a&is=6a8620aa&hm=c742f9c8774c7d7fc021eebe87108083a32ef93a9fbf76875f3aadc3df2bad54&',
+
+    GAMINGBUNKER_BANNER:
+        'https://cdn.discordapp.com/attachments/1510626895627288676/1539748637444280361/ChatGPT_Image_30._Juli_2026_22_54_30.png?ex=6a87722a&is=6a8620aa&hm=e508d55be1912a5295f60d3b583d9ac8290f64816303913f9532e790df16518d&'
+
+};
